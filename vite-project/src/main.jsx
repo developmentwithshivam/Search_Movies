0cb9b458a96a58a04a409a9b7d2c39fac06b4263
developmentwithshivam <developmentwithshivam@gmail.com>
2025-05-03 00:17:56 +0530

    just commit

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
 import App from "./App.jsx";
@@ -10,7 +10,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
     <Provider store={store}>
-    <App />
+    <HashRouter>
+  <App />
+</HashRouter>
   </Provider>,
     </BrowserRouter>
   </StrictMode>
