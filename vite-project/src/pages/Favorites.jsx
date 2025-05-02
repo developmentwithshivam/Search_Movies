3d418ee9cd13969cf3fdda458c55e6ca18c52361
developmentwithshivam <developmentwithshivam@gmail.com>
2025-05-03 00:58:04 +0530

    commit 9

--- vite-project/src/pages/Favorites.jsx
+++ vite-project/src/pages/Favorites.jsx
@@ -6,17 +6,20 @@ function Favorites() {
   console.log(savedData);
   
   return (
-     <div className={styles.container}>
-          {savedData.map((item) => (
-              <div key={item.imdbID} className={styles.card}>
-              <img src={item.Poster} alt={item.Title} className={styles.poster} />
-                <div className={styles.details}>
-                    <h3>{item.Title}</h3>
-                    <p>{item.Year}</p>
-                </div>
-             </div>
-        ))}
+    savedData.length > 0?<> <div className={styles.container}>
+    {savedData.map((item) => (
+        <div key={item.imdbID} className={styles.card}>
+        <img src={item.Poster} alt={item.Title} className={styles.poster} />
+          <div className={styles.details}>
+              <h3>{item.Title}</h3>
+              <p>{item.Year}</p>
           </div>
+       </div>
+  ))}
+    </div></>:<>
+    <h1>Loding..</h1>
+    </>
+    
   )
 }
 
